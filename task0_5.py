903d32fd56b0a78cfa06257a0a19a1ba75c7115d
danVolt <subham.kandankel10@gmail.com>
2020-04-29 19:28:55 +0530

    Update task0_5.py

--- task0_5.py
+++ task0_5.py
@@ -1,6 +1,7 @@
-n=int(input("Enter a no: "))
+print("ENTER THE INTERVAL: ")
+i=int(input("Enter the first number of the interval: "))
+n=int(input("Enter the last number of the interval: "))
 n=n+1
-i=1
 m=range(n)
 def prime(x):
     y=x//2
@@ -12,7 +13,19 @@ def prime(x):
         y=y-1
     if t==0:
         print(x)
-print("Prime numbers are : ")
-while i<n:
-    prime(m[i])
-    i=i+1
+print("Prime numbers in the given interval : ")
+for e in range(i, n):
+    prime(m[e])
+
+q=int(input("Insert a number to check whether it is prime or not: "))
+l=q//2
+c=0
+while l>1:
+    z=q%l
+    if z==0:
+        c=1
+    l=l-1
+if c==0:
+    print(q," is a prime number.")
+else:
+    print(q," is not a prime number.")
